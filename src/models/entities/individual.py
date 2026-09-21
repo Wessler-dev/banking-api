89fe8fbfff7cbf4552c79562a1011b5361b747fc
Wessler-dev <alextadeu062@gmail.com>
2026-09-21 17:38:45 -0300

    feat: Implementing legal entity

--- src/models/entities/individual.py
+++ src/models/entities/individual.py
@@ -8,7 +8,10 @@ class Individual(Base):
     monthly_income = Column(Float, nullable=False)
     age = Column(BIGINT, nullable=False)
     full_name = Column(String(150), nullable= True)
-    phone_number = Column(int,nullable=True)
+    phone_number = Column(String,nullable=True)
     email = Column(String(150),nullable=True)
     category = Column(String(150), nullable=True)
     balance = Column(Float,nullable=False)
+
+    def __repr__(self):
+        return f"[name={self.full_name,},email={self.email}, catetory={self.category}]"
---
+++ src/models/entities/legal_entity.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, BIGINT, Float
+from src.models.settings.base import Base
+
+class LegalEntity(Base):
+    __tablename__ = "Legal Entity"
+
+    id = Column(BIGINT, primary_key=True)
+    revenue = Column(Float, nullable=False)
+    age = Column(BIGINT,nullable=False)
+    trade_name = Column(String, nullable=True)
+    phone_number = Column(String, nullable=True)
+    corporate_email = Column(String, nullable=True)
+    category = Column(String, nullable=False)
+    balance = Column(Float, nullable=False)
+
+    def __repr__(self):
+        return f"[name={self.trade_name}, email={self.corporate_email}, balance={self.balance}]"
