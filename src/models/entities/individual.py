13d3907317b5ccd5e3a9dc94d5ae3b8a53018927
Wessler-dev <alextadeu062@gmail.com>
2026-09-21 16:53:59 -0300

    feat: implement individual entity

---
+++ src/models/entities/individual.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, BIGINT, Float
+from src.models.settings.base import Base
+
+class Individual(Base):
+    __tablename__ = "individual"
+
+    id = Column(BIGINT, primary_key=True)
+    monthly_income = Column(Float, nullable=False)
+    age = Column(BIGINT, nullable=False)
+    full_name = Column(String(150), nullable= True)
+    phone_number = Column(int,nullable=True)
+    email = Column(String(150),nullable=True)
+    category = Column(String(150), nullable=True)
+    balance = Column(Float,nullable=False)
